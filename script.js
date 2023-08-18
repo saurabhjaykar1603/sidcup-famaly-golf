82130719aa844ae8d945e6dff4a5e105674328dc
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-08-18 12:27:11 +0530

    feat - gsap scroll-tigger added to navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+gsap.to("#nav",{
+    backgroundColor:'#000',
+    duration:0.5,
+    height: '100px',
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end : "top -11%",
+        scrub:1
+
+    }
+})
