3d91b9c34dc28b201813446dcecbf4d43ec5d105
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-08-18 16:40:10 +0530

    feat - gsap scroll-tigger added to main section

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-gsap.to("#nav",{
-    backgroundColor:'#000',
-    duration:0.5,
+gsap.to("#nav", {
+    backgroundColor: '#000',
+    duration: 0.5,
     height: '100px',
-    scrollTrigger:{
-        trigger:"#nav",
-        scroller:"body",
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
         // markers:true,
-        start:"top -10%",
-        end : "top -11%",
-        scrub:1
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1
 
     }
+})
+
+gsap.to("#main", {
+    backgroundColor: '#000',
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        marker:true,
+        start:"top- -25%",
+        end: "top - 70%",
+        scrub: 2
+
+    }
+
 })
